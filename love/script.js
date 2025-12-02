c9bae5e85b69ef05d2d4d6689efb9693b06768a7
r <rin5uron@gmail.com>
2025-12-02 21:33:57 +0900

    思いつき

---
+++ love/script.js
@@ -0,0 +1,27 @@
+const mainButton = document.getElementById('mainButton');
+const message = document.getElementById('message');
+let count = 0;
+mainButton.addEventListener("click", function() {
+    count++;
+    message.textContent = `押された回数：${count} 回`;
+});
+
+const triggers1 = [
+    "暇だったらこのボタン押してね？",
+    "さみしくなったらこのボタン押してね？",
+    "私のこと思い出したらこのボタン押してね？",
+    // ...
+  ];
+
+  const messages1 = [
+    "もう眠いから一緒に寝よ〜",
+    "私に会いたい？？ごはんいこ！予定送って〜！",
+    "今日食べたものの写真送って📷✨",
+    "最近おすすめの映画かアニメ教えて〜",
+    "声聞きたい！電話して？",
+    "いま何してるの？",
+    "いまお風呂入ってる、いそがしい",
+    "おなかすいた！飯テロしてきて？",
+    // ...
+  ];
+  
